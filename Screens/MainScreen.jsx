f9fbe2df0e70d6cbf6805ce88fbac4002da070d2
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-04-08 17:02:02 -0400

    playing around native

--- Screens/MainScreen.jsx
+++ Screens/MainScreen.jsx
@@ -9,12 +9,15 @@ const MainScreen = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
     { key: "home", title: "Home", icon: "home" },
-    { key: "profile", title: "Profile", icon: "file" },
+    { key :"search", title:"Search", icon: "file-search"},
+    { key: "profile", title: "Profile", icon: "account" },
+    { key: 'settings', title: 'Settings', icon:'cogs'},
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home: HomeRoute,
     profile: ProfileRoute,
+    settings:ProfileRoute
   });
 
   return (
